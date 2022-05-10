dbd6e945b9ac930d55aa3af182b1a3ce66ed879a
Kevin Tamaki <tamakikevin@gmail.com>
2022-05-09 18:10:43 -0700

    building out the add to cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,39 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    // find if cartItems equals productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+    // If found, increment quantity
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => 
+        cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        );
+    }
+    // return new array with modified cartItems or new cart item
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    // Include what you want to expose in the context here: 
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
